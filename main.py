9ae29016834f2232d705b37b7aec1181bd21824f
daithileonard <DAITHILEONARD@GMAIL.COM>
2023-04-19 16:44:02 +0100

    Add header, footer and total price

--- main.py
+++ main.py
@@ -40,8 +40,24 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+    total = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total), border=1, ln=1)
+
+    # footer
+    # total price
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The Total Sum price is {total}", ln=1)
+    # company info
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=30, h=8, txt=f"DL Ltd")
+    pdf.image("images/pythonhow.png", w=10)
     pdf.output(f"PDFs/{filename}.pdf")
-    print(df)
-    print(invoice_nr)
+
 
 
